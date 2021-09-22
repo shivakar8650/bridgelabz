afc22576475f6b9dd67ca9afd4a4be2ad7578c24
shivakar8650 <shivakar.up99@gmail.com>
2021-09-23 00:28:12 +0530

    day 1

---
+++ Helloworld.java
@@ -0,0 +1,15 @@
+
+public class Helloworld {
+
+     public static void main(String arg[]){
+        while(true){
+          System.out.println("Hello world");
+          try{
+               Thread.sleep(60000);
+          }catch(Exception ex){
+              System.out.println(ex);
+          }
+      }
+
+   }
+}
